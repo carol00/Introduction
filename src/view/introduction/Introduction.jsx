ab0fe596cb21079520c8ecc6abd4e34caf52c996
unknown <carolhsu@yile.com.tw>
2021-11-26 14:52:42 +0800

    [新增] 名稱

--- src/view/introduction/Introduction.jsx
+++ src/view/introduction/Introduction.jsx
@@ -1,9 +1,24 @@
 import styles from './introduction.module.sass'
+import Image from '../../avatar.jpg'
+
+function About() {
+  return (
+    <div className={styles.about}>
+      <div className={styles.pic}>
+        <img src={Image} alt="C" />
+      </div>
+      <div className={styles.name}>
+        <h3>許佩蓉</h3>
+        <p>Carol</p>
+      </div>
+    </div>
+  )
+}
 
 export default function Introduction() {
   return (
     <div className={styles.root}>
-      Introduction Page
+      <About />
     </div>
   )
 }
