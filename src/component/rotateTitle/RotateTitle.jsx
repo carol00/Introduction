2d89f8c134d9516f0733b3c704d19d902dff9969
unknown <carolhsu@yile.com.tw>
2021-11-30 17:37:38 +0800

    [新增] 翻牌動畫

--- src/component/rotateTitle/RotateTitle.jsx
+++ src/component/rotateTitle/RotateTitle.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import styles from './rotateTitle.module.sass'
 
 export default function RotateTitle(props) {
   const [ title, setTitle ] = useState(props.title)
   const [ flipped, setFlipped ] = useState(false)
+  const [ rotateStyle, setRotateStyle ] = useState(styles.content)
+
   const changeState = () => {
     if (flipped) return
     setTitle(props.content)
+    setRotateStyle(`${styles.flip} ${styles.content}`)
     setFlipped(!flipped)
   }
 
   return (
-    <div onClick={() => changeState()} ><span>{title}</span></div>
+    <div className={styles.root}>
+      <div className={styles.wrap}>
+        <div className={rotateStyle} onClick={() => changeState()}>
+          <div className={styles.card}><span>{title}</span></div>
+          <div className={styles.card}><span>{title}</span></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
