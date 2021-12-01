dc96c4314d3690ee1aa068155cc744ce31c56d4b
unknown <carolhsu@yile.com.tw>
2021-12-01 17:35:50 +0800

    [修改] class判斷改用clsx

--- src/component/rotateTitle/RotateTitle.jsx
+++ src/component/rotateTitle/RotateTitle.jsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './rotateTitle.module.sass'
+import clsx from 'clsx'
 
 export default function RotateTitle(props) {
   const [ title, setTitle ] = useState(props.title)
   const [ flipped, setFlipped ] = useState(false)
-  const [ rotateStyle, setRotateStyle ] = useState(styles.content)
 
   const changeState = () => {
     if (flipped) return
     setTitle(props.content)
-    setRotateStyle(`${styles.flip} ${styles.content}`)
     setFlipped(!flipped)
   }
 
   return (
     <div className={styles.root}>
       <div className={styles.wrap}>
-        <div className={rotateStyle} onClick={() => changeState()}>
+        <div className={clsx(styles.content, flipped && styles.flip)} onClick={() => changeState()}>
           <div className={`${styles.card} ${styles.front}`}><span>{title}</span></div>
           <div className={`${styles.card} ${styles.back}`}><span>{title}</span></div>
         </div>
