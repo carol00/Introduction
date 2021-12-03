ef391a92132363fb8cfee5cd4ea829beb810488a
unknown <carolhsu@yile.com.tw>
2021-12-03 12:12:43 +0800

    [新增] 燈泡開關觸發

--- src/component/lightBulb/LightBulb.jsx
+++ src/component/lightBulb/LightBulb.jsx
@@ -1,12 +1,19 @@
 import styles from './lightBulb.module.sass'
 import LightbulbIcon from '@mui/icons-material/Lightbulb'
 import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices'
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
 
-export default function LightBulb() {
+export default function LightBulb(switcher) {
+  const open = switcher.switcher
   return (
     <div className={styles.root}>
-      <LightbulbIcon className={styles.lightBulbIcon} />
-      <ElectricalServicesIcon className={styles.electricalIcon} />
+      <LightbulbIcon className={clsx(styles.lightBulbIcon, open && styles.openLightBulb)} />
+      <ElectricalServicesIcon className={clsx(styles.electricalIcon, open && styles.openElectrical)} />
     </div>
   )
+}
+
+LightBulb.propTypes = {
+  switcher: PropTypes.bool
 }
--- src/view/introduction/Introduction.jsx
+++ src/view/introduction/Introduction.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import styles from './introduction.module.sass'
 import Image from '../../assets/avatar.jpg'
 import RotateTitle from '../../component/rotateTitle/RotateTitle'
@@ -30,11 +32,21 @@ function Description() {
 }
 
 export default function Introduction() {
+  const rotateTitleTotal = useSelector(state => state.rotateTitle.total)
+  const [ fullState, setFullState ] = useState(false)
+  const fullAmount = 3
+
+  useEffect(() => {
+    if (rotateTitleTotal === fullAmount) {
+      setFullState(true)
+    }
+  }, [fullState, rotateTitleTotal])
+
   return (
     <div className={styles.root}>
       <About />
       <div className={styles.content}>
-        <LightBulb />
+        <LightBulb switcher={fullState} />
         <Description />
       </div>
     </div>
