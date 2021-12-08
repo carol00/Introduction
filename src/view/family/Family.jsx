c037f40b5e831e18839bbf8b46b302e6d938fa20
unknown <carolhsu@yile.com.tw>
2021-12-08 14:13:12 +0800

    [新增] 生活圖component

--- src/view/family/Family.jsx
+++ src/view/family/Family.jsx
@@ -2,16 +2,16 @@ import Avatar from '../../component/avatar/Avatar'
 import Image from '../../assets/avatar.jpg'
 import styles from './family.module.sass'
 import Icon from '@mdi/react'
-import { mdiHomeAnalytics } from '@mdi/js';
+import { mdiHomeAnalytics } from '@mdi/js'
 
 const AvatarArea = () => {
   const imgSize = 75
   return (
-    <div className={styles.avatarArea}>
+    <div className={styles.avatarContainer}>
       <div className={styles.avatarContent}>
           <Avatar img={Image} width={imgSize} height={imgSize} />
-          <Avatar img={Image} width={imgSize} height={imgSize}/>
-          <Avatar img={Image} width={imgSize} height={imgSize}/>
+          <Avatar img={Image} width={imgSize} height={imgSize} />
+          <Avatar img={Image} width={imgSize} height={imgSize} />
       </div>
       <div className={styles.iconContent}>
         <div className={styles.scaleUnbalanced}>
@@ -22,11 +22,43 @@ const AvatarArea = () => {
   )
 }
 
+const picContent = [
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'},
+  {title: '12/4', src: '../../assets/family/f1.jpg'}
+]
+
+const LifePhoto = () => {
+  return (
+    <div className={styles.festivalContainer}>
+      {picContent.map((value, index) => (
+        <div key={index} className={styles.picContent}>
+          <p className={styles.title}>{value.title}</p>
+          <div><img src={value.src} alt="" /></div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export default function Family() {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
         <AvatarArea />
+        <LifePhoto />
       </div>
     </div>
   )
---
+++ src/view/family/familySlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const familySlice = createSlice({
+  name: 'familySlice',
+  initialState: {
+    pic: []
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+
+    )
+  }
+})
