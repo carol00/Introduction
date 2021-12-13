170bf09b1c28cec5f0b336703105c6fbb1f576e7
unknown <carolhsu@yile.com.tw>
2021-12-13 13:34:53 +0800

    [新增] 在同一畫面 點擊時切換下一component

--- src/view/handMade/HandMade.jsx
+++ src/view/handMade/HandMade.jsx
@@ -1,5 +1,29 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import { useTransition, animated, useSpringRef } from '@react-spring/web'
+import styles from './handMade.module.sass'
+
+const pages = [
+  ({ style }) => React.createElement(animated.div, { style: Object.assign(Object.assign({}, style), { background: 'lightpink' }) }, "A"),
+  ({ style }) => React.createElement(animated.div, { style: Object.assign(Object.assign({}, style), { background: 'lightblue' }) }, "B"),
+  ({ style }) => React.createElement(animated.div, { style: Object.assign(Object.assign({}, style), { background: 'lightgreen' }) }, "C"),
+];
+
 export default function HandMade() {
-  return (
-    <div>Hand Made</div>
-  )
+  const [index, set] = useState(0);
+  const onClick = useCallback(() => set(state => (state + 1) % 3), []);
+  const transRef = useSpringRef();
+  const transitions = useTransition(index, {
+    ref: transRef,
+    keys: null,
+    from: { opacity: 0, transform: 'translate3d(0,100%,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0,0%,0)' },
+    leave: { opacity: 0, transform: 'translate3d(0,-50%,0)' },
+  });
+  useEffect(() => {
+    transRef.start();
+  }, [index, transRef]);
+  return (React.createElement("div", { className: `${styles.root}`, onClick: onClick }, transitions((style, i) => {
+    const Page = pages[i];
+    return React.createElement(Page, { style: style });
+  })));
 }
