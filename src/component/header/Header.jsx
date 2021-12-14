e6a447cfc5cac44ea011a02ea6411e722bb6f851
unknown <carolhsu@yile.com.tw>
2021-12-14 18:02:37 +0800

    [新增] header active style

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation  } from "react-router-dom";
 import styles from './header.module.sass'
+import clsx from "clsx";
 
 const title = [
   {name: 'Introduction', to: '/'},
@@ -10,12 +11,14 @@ const title = [
 ]
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <div className={styles.root}>
       <ul>
         {title.map((value, index) => (
           <li key={index}>
-            <Link to={value.to}>{value.name}</Link>
+            <Link to={value.to}  className={clsx(location.pathname === `${value.to}` && styles.active)}>{value.name}</Link>
           </li>
         ))}
       </ul>
