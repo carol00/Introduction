15c86298547e661e031f375da5de40ea0f95adf2
unknown <carolhsu@yile.com.tw>
2021-12-14 14:49:13 +0800

    [新增] 作品圖component

---
+++ src/component/artWorkPic/ArtWorkPic.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import styles from './artWorkPic.module.sass'
+
+export default function ArtWorkPic({img, title, width, height}) {
+  return (
+    <div className={styles.content} style={{width: width, height: height}}>
+      <img src={img} alt="" />
+      <div className={styles.title}>
+        <p>{title}</p>
+      </div>
+    </div>
+  )
+}
+
+ArtWorkPic.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+}
