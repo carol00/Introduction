b7b77b4757fdc3432872f4d850381f5c5fc2c275
unknown <carolhsu@yile.com.tw>
2021-12-23 14:49:16 +0800

    [修改] family圖片呈現改為相片牆

--- src/view/family/Family.jsx
+++ src/view/family/Family.jsx
@@ -1,45 +1,52 @@
-import Avatar from '../../component/avatar/Avatar'
+// import Avatar from '../../component/avatar/Avatar'
 import styles from './family.module.sass'
-import Icon from '@mdi/react'
+// import Icon from '@mdi/react'
 import { useEffect } from 'react'
-import { mdiHomeAnalytics } from '@mdi/js'
+// import { mdiHomeAnalytics } from '@mdi/js'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPhotoData } from '../../redux/familySlice'
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
-const AvatarArea = () => {
-  const avatarData = useSelector(state => state.family.avatar)
-  const imgSize = 75
-  return (
-    <div className={styles.avatarContainer}>
-      <div className={styles.avatarContent}>
-        {avatarData.map((value, index) => (
-          <Avatar key={index} img={value.src} width={imgSize} height={imgSize} />
-        ))}
-      </div>
-      <div className={styles.iconContent}>
-        <div className={styles.scaleUnbalanced}>
-          <Icon path={mdiHomeAnalytics } />
-        </div>
-      </div>
-    </div>
-  )
-}
+// const AvatarArea = () => {
+//   const avatarData = useSelector(state => state.family.avatar)
+//   const imgSize = 75
+//   return (
+//     <div className={styles.avatarContainer}>
+//       <div className={styles.avatarContent}>
+//         {avatarData.map((value, index) => (
+//           <Avatar key={index} img={value.src} width={imgSize} height={imgSize} />
+//         ))}
+//       </div>
+//       <div className={styles.iconContent}>
+//         <div className={styles.scaleUnbalanced}>
+//           <Icon path={mdiHomeAnalytics } />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 const LifePhoto = () => {
   const photoData = useSelector(state => state.family.photo)
 
   return (
-    <div className={styles.festivalContainer}>
-      {photoData.map((value, index) => (
-        <div key={index} className={styles.picContent}>
-          <div>
-            <img src={value.src} alt="" />
-            {value.title &&
-              <p className={styles.title}>{value.title}</p>
-            }
-          </div>
-        </div>
-      ))}
+    <div>
+      <Box sx={{ width: 800, height: 700, overflowY: 'auto' }}>
+        <ImageList variant="masonry" cols={6} gap={8}>
+          {photoData.map((item, index) => (
+            <ImageListItem key={index}>
+              <img
+                src={item.src}
+                srcSet={item.src}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
     </div>
   )
 }
@@ -54,7 +61,7 @@ export default function Family() {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-        <AvatarArea />
+        {/* <AvatarArea /> */}
         <LifePhoto />
       </div>
     </div>
