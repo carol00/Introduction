2954dda78e4403cb05cf7785abb713742529dd19
carol00 <carol1330s@gmail.com>
2022-02-14 21:43:41 +0800

    [新增] family影片

--- src/view/family/Family.jsx
+++ src/view/family/Family.jsx
@@ -1,9 +1,21 @@
 import styles from './family.module.sass'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPhotoData } from '../../redux/familySlice'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import boboVideo from '../../assets/bobo.mp4'
+import pippyVideo from '../../assets/pippy.mp4'
+// import Icon from '@mdi/react'
+// import { mdiDog } from '@mdi/js'
 
 const srcset = (image, size, rows = 1, cols = 1) => {
   return {
@@ -18,22 +30,82 @@ const LifePhoto = () => {
   const photoData = useSelector(state => state.family.photo)
 
   return (
-    <ImageList
-      sx={{ width: 650, height: 600 }}
-      variant="quilted"
-      cols={4}
-      rowHeight={121}
-    >
-      {photoData.map((item) => (
-        <ImageListItem key={item.src} cols={item.cols || 1} rows={item.rows || 1}>
-          <img
-            {...srcset(item.src, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <div>
+      <ImageList
+        sx={{ width: 650, height: 600 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
+        {photoData.map((item) => (
+          <ImageListItem key={item.src} cols={item.cols || 1} rows={item.rows || 1}>
+            <img
+              {...srcset(item.src, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </div>
+  )
+}
+
+const ShowDialog = (props) => {
+  const { dogName, videoName } = props
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('lg'));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className={styles.dialog}>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        {dogName}
+      </Button>
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {/* <div className={styles.title}>
+            <Icon path={mdiDog} />
+          </div> */}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <video controls>
+              <source src={videoName} type="video/mp4" />
+            </video>
+          </DialogContentText>
+        </DialogContent>
+        {/* <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Disagree
+          </Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions> */}
+      </Dialog>
+    </div>
+  )
+}
+
+const LifeVideo = () => {
+  return (
+    <div className={styles.videoContent}>
+      <ShowDialog dogName={'Pippy'} videoName={pippyVideo} />
+      <ShowDialog dogName={'BoBo'} videoName={boboVideo} />
+    </div>
   )
 }
 
@@ -49,6 +121,7 @@ export default function Family() {
       <div className={styles.container}>
         {/* <AvatarArea /> */}
         <LifePhoto />
+        <LifeVideo />
       </div>
     </div>
   )
